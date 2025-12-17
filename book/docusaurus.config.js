26de8b2c0916dd6893f978fdc14f7d0bcd8babfc
AyeshaMaryam-1 <maryamayesha681@gmail.com>
2025-12-17 19:59:49 +0500

    Update edit URL in docusaurus.config.js

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -47,10 +47,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          routeBasePath: '/',
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AyeshaMaryam-1/physical-ai-humanoid-robotics-book/tree/main/book/',
         },
         blog: false, // Disable blog for textbook companion site
         theme: {
